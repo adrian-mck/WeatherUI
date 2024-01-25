9c0ab8391cd0d2af70c9233e5ebbd557749d3774
Adrian Mück <adrian.mueck@student.fhws.de>
2024-01-25 04:12:13 +0100

    Initial Commit

---
+++ WeatherUI/WeatherUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherUIApp.swift
+//  WeatherUI
+//
+//  Created by Adrian Mück on 25.01.24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
